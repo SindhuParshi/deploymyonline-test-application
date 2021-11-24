09af1347519f60041d225a1bf788b97c0fd3763e
Sindhu <sindhu.parshi@gmail.com>
2021-11-24 17:20:36 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BackgroundDirective } from './background.directive';
+import { WelcomeComponent } from './welcome/welcome.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    BackgroundDirective,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
